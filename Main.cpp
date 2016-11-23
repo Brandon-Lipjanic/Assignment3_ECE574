79aeaac3e1ca463cf157a8dc50e145a7593ab1e8
abigailfrancis <abigailfrancis@email.arizona.edu>
2016-11-23 10:42:04 -0700

    planz

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,12 @@
 int main(int argc, char* argv[]) {
 	//ALAP
 	//ASAP
-	/*receive latency, vector(Node*)*/
+	
 
 	//determine_width
 	//determine_probability
 	//determine_type distribution
+
 	//self_force
 	//predecessor_force
 	//successor_force
